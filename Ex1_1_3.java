c8e67e755d07fbd6f7cd7bd0c901fadf81b99f2d
David Leung <david@davidslab.com>
2014-04-06 17:39:01 +0800

    Solution to exercise 1.1.3

---
+++ Ex1_1_3.java
@@ -0,0 +1,28 @@
+/*********************************************************************
+ * Solution to exercises in Algorithms Fourth Edition
+ * by Rovert Sedgewick and Kevin Wayne
+ * 
+ * Exercise 1.1.3
+ * 
+ * The solution provided here is capable of handling arbitrarily long
+ * inputs from the command line.
+ * 
+ * Copyright 2014 David Leung
+ * Released under the MIT license.
+ ********************************************************************/
+
+public class Ex1_1_3 {
+
+    public static void main(String[] args) {
+        
+        int firstInput = Integer.parseInt(args[0]);
+        
+        for (int i = 1; i < args.length; i++) {
+            if (Integer.parseInt(args[i]) != firstInput) {
+                StdOut.println("not equal");
+                return;
+            }
+        }
+        StdOut.println("equal");
+    }
+}
