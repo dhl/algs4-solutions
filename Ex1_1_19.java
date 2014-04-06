5893dac21da716b38b451954f9e897b4d17faee4
David Leung <david@davidslab.com>
2014-04-06 22:09:16 +0800

    Exercise 1.1.19

---
+++ Ex1_1_19.java
@@ -0,0 +1,23 @@
+public class Ex1_1_19 {
+    public static long fibonacci(int N) {
+        assert N >= 0 : "N must be larger than or equal to zero";
+        int len = N < 2 ? 3 : N+1;
+        long[] fib = new long[len];
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        for (int i = 2; i < len; i++) {
+            fib[i] = fib[i-1]+fib[i-2];
+        }
+        
+        return fib[N];
+    }
+    
+    public static void main(String[] args) {
+        assert fibonacci(0) == 0;
+        assert fibonacci(1) == 1;
+        assert fibonacci(10) == 55;
+        assert fibonacci(12) == 144;
+        assert fibonacci(20) == 6765;
+    }
+}
